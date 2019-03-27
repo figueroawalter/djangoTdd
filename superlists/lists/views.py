78c94f77efde176d757fc06e442ba79f76034059
zeus <figueroawalter@gmail.com>
2019-03-27 10:08:25 -0300

    Functional test now checks we can input a to-do item

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return HttpResponse('<html><title>To-Do lists</title><h1>To-Do</h1><input id="id_new_item" placeholder="Enter a to-do item"></html>')
