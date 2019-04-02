f267b95bc9b34c6efbf2c74d8626edf161fe6510
zeus <figueroawalter@gmail.com>
2019-04-02 15:57:18 -0300

    Add a URL for creating a new list via POST

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -1,7 +1,7 @@
 """superlists URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://    docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from lists import views
 
 urlpatterns = [
-    path('', views.home_page, name="home"),
-    path('lists/the-only-list-in-the-world/', views.view_list, name="view_list"),
+    re_path(r'^$', views.home_page, name='home'),
+    re_path(r'^lists/new$', views.new_list, name='new_list'),
+    re_path(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
 ]
